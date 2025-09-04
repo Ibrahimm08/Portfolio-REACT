4acda049c0c6dd63f947c7f6724dd03c2f3ab00e
Ibrahimm08 <imaqsood360@outlook.com>
2025-09-04 12:54:21 +0100

    Added Footer style and component

---
+++ src/Footer.jsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>{/*Icons such as github and linkedin will go here*/}</p>
+      </div>
+      <div>
+        <ul>
+          {/*Contact info and name will go here*/}
+          <p>
+            <strong>Name: </strong>
+          </p>
+          <p>
+            <strong>Email: </strong>
+          </p>
+          <p>
+            <strong>Phone: </strong>
+          </p>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
