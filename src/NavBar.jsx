2ce50b3529b1e1dbade681af4edc7b1d7a53895d
Ibrahimm08 <imaqsood360@outlook.com>
2025-09-04 11:30:15 +0100

    Added NavBar style and components

---
+++ src/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <a href="">Home</a>
+        <a href="">Projects</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
