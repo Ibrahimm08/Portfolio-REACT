9676fbd726a88eb621ed9d4af31269eecd599b0a
Ibrahimm08 <imaqsood360@outlook.com>
2025-09-04 12:37:57 +0100

    Added hover effects and styling

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,11 +1,29 @@
+import "./NavBar.css";
+
 const NavBar = () => {
   return (
     <div className="navbar">
       <ul>
-        <a href="">Home</a>
-        <a href="">Projects</a>
-        <a href="">About</a>
-        <a href="">Contact</a>
+        <p>
+          <a data-text="Home" href="">
+            Home
+          </a>
+        </p>
+        <p>
+          <a data-text="Projects" href="">
+            Projects
+          </a>
+        </p>
+        <p>
+          <a data-text="About" href="">
+            About
+          </a>
+        </p>
+        <p>
+          <a data-text="Contact" href="">
+            Contact
+          </a>
+        </p>
       </ul>
     </div>
   );
