4915aafb79e9f222436b39aa93ed8d75fc5993c9
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MBP.localdomain>
2025-09-18 14:02:48 +0100

    Added popup effect for projects

--- src/home-page/components/FeaturedProject.jsx
+++ src/home-page/components/FeaturedProject.jsx
@@ -1,7 +1,40 @@
 import "../styles/FeaturedProject.css";
 import image from "../images/arrow.svg";
+import { useEffect, useRef, useState } from "react";
 
 const FeaturedProject = () => {
+  const [inView, setInView] = useState(false);
+  const refference = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setInView(entry.isIntersecting);
+      },
+      { threshold: 0.1 } // How much % element has to be in view
+    );
+
+    if (refference.current) {
+      observer.observe(refference.current);
+    }
+
+    return () => {
+      if (refference.current) {
+        observer.unobserve(refference.current);
+      }
+    };
+  }, []);
+
+  // In view play animation to summon featured projects 
+  useEffect(() => {
+    console.log(inView);
+    if (inView){
+      document.documentElement.style.setProperty("--ft-project-scale", "100%");
+    } else{
+      document.documentElement.style.setProperty("--ft-project-scale", "0%");
+    }
+  }, [inView]);
+
   return (
     <div className="featured container">
       <div className="featured-projects">
@@ -16,7 +49,7 @@ const FeaturedProject = () => {
           <div className="ft-project-arrow-cursor-cursor-inner"></div>
         </div>
       </div>
-      <div className="ft-project-desc">
+      <div ref={refference} className="ft-project-desc">
         <h1>Header</h1>
         <p>Content</p>
       </div>
