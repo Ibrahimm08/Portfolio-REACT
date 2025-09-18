ded6c10eccc3c70f37360851768d02761079f490
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MacBook-Pro.local>
2025-09-18 11:27:21 +0100

    Added layout for featured projects

--- src/home-page/components/FeaturedProject.jsx
+++ src/home-page/components/FeaturedProject.jsx
@@ -1,7 +1,25 @@
 import "../styles/FeaturedProject.css";
+import image from "../images/arrow.svg";
 
 const FeaturedProject = () => {
-  return <div className="featured container"></div>;
+  return (
+    <div className="featured container">
+      <div className="featured-projects">
+        <div className="ft-project projecta"></div>
+        <div className="ft-project projectb"></div>
+        <div className="ft-project projectc"></div>
+      </div>
+      <div className="ft-project-arrow">
+        <img src={image} alt="" />
+        <p>text</p>
+        <div className="ft-project-arrow-cursor"></div>
+      </div>
+      <div className="ft-project-desc">
+        <h1>Header</h1>
+        <p>Content</p>
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedProject;
