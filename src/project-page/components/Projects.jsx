456053948276152572ddca8ebe6916255c5f1036
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MBP.localdomain>
2025-10-02 09:43:54 +0100

    Stored api results in local storage

--- src/project-page/components/Projects.jsx
+++ src/project-page/components/Projects.jsx
@@ -6,12 +6,13 @@ const Projects = () => {
   const [filter, setFilter] = useState("all");
   const [expanded, setExpanded] = useState(null);
 
-
   // Add images using /images/${repo.name}.png
   // Add topics to bigbite repos
 
   // Risk of rate limit too many refreshes
   // Set username to Ibrahimm08
+
+  // Gets the repos where user contributed
   const checkOwner = async (username, repos) => {
     const contributed = [];
 
@@ -34,43 +35,58 @@ const Projects = () => {
     return contributed;
   };
 
+  // On mount get repositories from user and organisation
   useEffect(() => {
     const projectTopics = async () => {
       try {
-        const userResponses = await fetch(
-          "https://api.github.com/users/Ibrahimm08/repos"
-        );
-        const userRepos = await userResponses.json();
+        // Use local storage to prevent too many requests
+        if (localStorage.getItem("Ibrahimm08's repos") != null) {
+          console.log("Retrieved repos from storage");
+          setProjects(JSON.parse(localStorage.getItem("Ibrahimm08's repos")));
+        } else {
+          const userResponses = await fetch(
+            "https://api.github.com/users/Ibrahimm08/repos"
+          );
 
-        const orgResponses = await fetch(
-          "https://api.github.com/orgs/BB-WEX/repos"
-        );
-        const orgRepos = await orgResponses.json();
+          const userRepos = await userResponses.json();
 
-        const allRepos = [...userRepos, ...orgRepos];
+          const orgResponses = await fetch(
+            "https://api.github.com/orgs/BB-WEX/repos"
+          );
 
-        const checkedRepos = await checkOwner("Ibrahimm08", allRepos);
-        console.log("Checked Repos:", checkedRepos);
+          const orgRepos = await orgResponses.json();
 
-        const combinedRepos = await Promise.all(
-          checkedRepos.map(async (repo) => {
-            const topicResponse = await fetch(
-              // owner.login to get org and user
-              `https://api.github.com/repos/${repo.owner.login}/${repo.name}/topics`,
-              {
-                headers: {
-                  Accept: "application/vnd.github.mercy-preview+json",
-                },
-              }
-            );
-            const topicData = await topicResponse.json();
-            return { ...repo, topics: topicData.names };
-          })
-        );
+          const allRepos = [...userRepos, ...orgRepos];
 
-        setProjects(combinedRepos);
+          const checkedRepos = await checkOwner("Ibrahimm08", allRepos);
+          console.log("Checked Repos:", checkedRepos);
+
+          const combinedRepos = await Promise.all(
+            checkedRepos.map(async (repo) => {
+              const topicResponse = await fetch(
+                // owner.login to get org and user
+                `https://api.github.com/repos/${repo.owner.login}/${repo.name}/topics`,
+                {
+                  headers: {
+                    Accept: "application/vnd.github.mercy-preview+json",
+                  },
+                }
+              );
+
+              const topicData = await topicResponse.json();
+              return { ...repo, topics: topicData.names };
+            })
+          );
+
+          console.log("Stored repos in storage");
+          localStorage.setItem(
+            "Ibrahimm08's repos",
+            JSON.stringify(combinedRepos)
+          );
+          setProjects(combinedRepos);
+        }
       } catch (error) {
-        console.error("Couldn't fetch repos");
+        console.error("Couldn't fetch repos", error);
       }
     };
 
@@ -78,17 +94,22 @@ const Projects = () => {
   }, []);
 
   const handleFilter = (chosenFilter) => {
-    setFilter(chosenFilter);
+    if (chosenFilter == filter) {
+      setFilter("all");
+    } else {
+      setFilter(chosenFilter);
+    }
     setExpanded(null);
   };
 
-  const filterdProjects =
+  const filteredProjects =
     filter == "all"
       ? projects
       : projects.filter((project) =>
           project.topics?.includes(filter.toLowerCase())
         );
 
+  console.log(filteredProjects);
   return (
     <div className="projects container">
       <div className="projects searchbar">
@@ -110,7 +131,7 @@ const Projects = () => {
         </div>
       </div>
       <div className="projects-grid">
-        {filterdProjects.map((project, index) => (
+        {filteredProjects.map((project, index) => (
           <div
             key={project.id}
             className={`project-card ${expanded == index ? "expanded" : ""}`}
